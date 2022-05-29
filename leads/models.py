e4e5f63fa184db2b82e061bd6fa39a06a4c7f85e
JJohnyy <jan.jasa@email.cz>
2022-05-29 07:15:33 +0000

    preapring views

--- leads/models.py
+++ leads/models.py
@@ -16,4 +16,7 @@ class Lead(models.Model):
 
 class Agent(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.name
     
