c063ead9addc7fa919a777414e5d7d78b7fb0e0f
JJohnyy <jan.jasa@email.cz>
2022-05-29 06:35:15 +0000

    preaparing models user, lead, agent

--- BES_CRM/settings.py
+++ BES_CRM/settings.py
@@ -142,3 +142,5 @@ DEAFULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'leads.User'
