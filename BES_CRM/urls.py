d98fb2b4aa19319e7b2a3e9b5c0509150c57c837
JJohnyy <jan.jasa@email.cz>
2022-05-29 07:22:48 +0000

    updated urls

--- BES_CRM/urls.py
+++ BES_CRM/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .leads.views import home_page
+from leads.views import home_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
