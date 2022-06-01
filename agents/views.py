d7772e353baf1f10b9064f827cafa591a68bc8a2
JJohnyy <jan.jasa@email.cz>
2022-06-01 13:14:56 +0000

    preparing agent delete view, agent list and navbar

--- agents/views.py
+++ agents/views.py
@@ -26,6 +26,35 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent = form.save(commit=False)
         agent.organisation = self.request.user.user_profile
         agent.save()
+        return super(AgentCreateView, self).form_valid(form)
+
+
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = 'agents/agent_detail.html'
+    context_object_name = 'agent'
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = 'agents/agent_update.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse('agents:agent-list')
+
+    def get_queryset(self):
+        organisation = self.request.user.userprofile
+        return Agent.objects.filter(organisation=organisation)
+
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = 'agents/agent_delete.html'
+    context_object_name = 'agent'
+
+    def get_queryset(self):
+        return Agent.objects.all()
 
 
 
@@ -45,5 +74,4 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
 
 
 
-        
         return super(AgentCreateView, self).form_valid(form)
