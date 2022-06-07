27650e9ade4304f4f809d81ad263de0a20b892fa
JJohnyy <jan.jasa@email.cz>
2022-06-07 19:54:44 +0000

    jquery

--- static/js/main.js
+++ static/js/main.js
@@ -1,28 +1,34 @@
 
 
 $(document).ready(function () {
-    $('.navbar-link').click(function () {
-        $('.navbar-link').removeClass("active");
+    $('ul li').click(function () {
+        $('ul li').removeClass("active");
         $(this).addClass("active");
     });
 });
 
 
-$(document).ready(function () {
+/*$(document).ready(function () {
     $('.lead-detail-link-item').click(function () {
         // Removing class from all list elements
         $('.lead-detail-link-item').removeClass('active');
         // Adding class to clicked list element
         $(this).addClass('active');
     });
-});
+});*/
+
+/*$(".lead-detail-link-item").on("click", function(e){
+    e.preventDefault();
+    $(this).addClass("active"); 
+    $(this).siblings().removeClass("active");
+ });*/
 
 
 
 
 $(function () {
     count = 0;
-    wordsArray = ["BES CRM", "Bohemian Esteates", 'BES'];
+    wordsArray = ["BES CRM", "Bohemian Estates", 'BES'];
     setInterval(function () {
         count++;
         $("#heading").fadeOut(400, function () {
