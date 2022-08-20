f2a83ed07db5f81ff39ecf27ad64bf52759f6db7
JJohnyy <84328224+JJohnyy@users.noreply.github.com>
2022-08-20 15:26:49 +0200

    .

--- agents/models.py
+++ agents/models.py
@@ -5,10 +5,13 @@ from django.conf import settings
 # Create your models here.
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 class User(models.Model):
     is_organiser = models.BooleanField(default=True)
     is_agent = models.BooleanField(default=False)
 =======
+=======
+>>>>>>> parent of 4cd8c1d (deleted abstractuser)
 class User(AbstractUser):
     is_organiser = models.BooleanField(default=True)
     is_agent = models.BooleanField(default=True)
@@ -16,6 +19,9 @@ class User(AbstractUser):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+<<<<<<< HEAD
+>>>>>>> parent of 4cd8c1d (deleted abstractuser)
+=======
 >>>>>>> parent of 4cd8c1d (deleted abstractuser)
 
     def __str__(self):
