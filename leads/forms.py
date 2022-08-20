a76d50689391903ae5566993c4ebbff005fed9ed
JJohnyy <jan.jasa@email.cz>
2022-08-20 10:05:35 +0000

    moved UserCreationForm from leads to agens

--- leads/forms.py
+++ leads/forms.py
@@ -1,7 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from .models import Lead, Category
-from django.contrib.auth.forms import UserCreationForm, UsernameField
 
 
 User = get_user_model()
@@ -20,20 +19,13 @@ class LeadModelForm(forms.ModelForm):
             'description',
             'agent',
         )
-        
+
 
 class LeadForm(forms.Form):
     first_name = forms.CharField(max_length=20)
     last_name = forms.CharField(max_length=20)
     age = forms.IntegerField(min_value=0)
-
-
-class CustomUserCreationForm(UserCreationForm):
-    class Meta:
-        model = User
-        fields = ("username",)
-        field_classes = {"username": UsernameField}
-
+        
 
 class CategoryModelForm(forms.ModelForm):
     class Meta:
